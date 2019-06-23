a7c5b401910dab154b21d84f25d7fdb2aef6b701
Mate Rusz <ruszmate33@gmail.com>
2019-06-23 15:56:42 +0200

    newest

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native'
 import { Orban } from './Orban'
-import { Counter, num } from './Counter'
+//import Counter from './Counter'
 import { throwIfAudioIsDisabled } from 'expo-av/build/Audio/AudioAvailability'
 import {vibrate} from './utils'
 import Clock from './Clock'
@@ -12,17 +12,33 @@ export default class App extends React.Component {
       constructor() { 
         super()
         this.state = {
-            countWork: 20,
+            countWork: 25*60,
             countRest:0,
             started: false,
             workPhase: true,
             showForm: false,
+            hoursWork: 0,
+            minutesWork: 25,
+            secondsWork: 0,
+            hoursRest: 0,
+            minutesRest: 0,
+            secondsRest: 0,
         }
     }
-
+    convertTimer = () => {
+      this.setState(prevState => {
+          hoursWork: Math.floor((this.state.countWork % (60 * 60 * 24)) / (60 * 60));
+          minutesWork: Math.floor((this.state.countWork % (60 * 60)) / (60));
+          secondsWork: Math.floor(this.state.countWork % 60);
+          hoursRest: Math.floor((this.state.countRest % (60 * 60 * 24)) / (60 * 60));
+          minutesRest: Math.floor((this.state.countRest % (60 * 60)) / (60));
+          secondsRest: Math.floor(this.state.countRest % 60);
+      })
+    }
+    
     resetCounter = () => {
       this.setState(prevState => ({
-        countWork: 20,
+        countWork: 25*60,
         countRest:0,
         started: false,
         workPhase: true,
@@ -40,22 +56,30 @@ export default class App extends React.Component {
     toggleForm = () => {
       this.setState(prevState => ({showForm: !prevState.showForm}))
     }
+    /* 
+    updater = () => {
+      this.inc;
+      this.convertTimer;
+    }
+    */
 
     componentDidMount() {
-      setInterval(this.inc, 1000)
+      setInterval(this.inc, 1000);
+      //setInterval(this.convertTimer, 1000)
     }
 
     componentDidUpdate() {
       if (this.state.workPhase && this.state.countWork == 0) {
         vibrate();
         this.setState(prevState => ({
-          countRest: 20,
+          countRest: 5*60,
           workPhase: false,
+          
       }))
       } else if (!this.state.workPhase && this.state.countRest == 0) {
         vibrate();
         this.setState(prevState => ({
-          countWork: 20,
+          countWork: 25*60,
           workPhase: true,
         }))
         } 
@@ -66,11 +90,13 @@ export default class App extends React.Component {
         this.setState(prevState => ({
             countWork: prevState.countWork - 1,
         }))
+        this.convertTimer();
       } else if (!this.state.workPhase && this.state.started) {
         this.setState(prevState => ({
             countRest: prevState.countRest - 1,
-    }))
-  }
+            }))
+        this.convertTimer();
+    }
   }
    
 render() {
@@ -80,6 +106,8 @@ render() {
   return (
     <View style={styles.container}>
         <Text style={styles.counterWork}>{this.state.countWork}</Text>
+        <Text style={styles.counterWork}>{this.state.hoursWork}:{this.state.minutesWork}:{this.state.secondsWork}</Text>
+        <Text style={styles.counterRest}>{this.state.hoursRest}:{this.state.minutesRest}:{this.state.secondsRest}</Text>
         <Text style={styles.counterRest}>{this.state.countRest}</Text>
         <View>
           <Button title="Start" onPress={this.letsStart} />
